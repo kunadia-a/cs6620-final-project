a4cd58569b29dd9ebdc4e9c3c5cd28a75c4726b6
ChanceLamberth <Lamberth.c@northeastern.edu>
2022-04-28 09:49:20 -0500

    Updated stock price card

--- src/API/stocksAPI.jsx
+++ src/API/stocksAPI.jsx
@@ -1,25 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
 const StockAPI = (stockTicker) => {
+    const [stockPrice, setStockPrice] = useState();
+    const [stockLongName, setStockName] = useState();
+    const [stockSymbol, setStockSymbol] = useState();
+
     const ticker = stockTicker.stockTicker;
-    // console.log("TICKER!", ticker);
     let v1 = 'https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols='
     let v2 = ticker;
     let v3 = '%2CBTC-USD%2CEURUSD%3DX'
-    // console.log("URL!", v1 + v2 + v3);
 
     const URL = v1 + v2 + v3;
-    // console.log("This is value:", ticker)
-    const APILINK = "https://yfapi.net/v11/finance/quoteSummary/AAPL";
     const APIKEY = "soCvVBKHIqa4w8UJXu7fS6g6Vf2dDJo03NAzuBcu";
 
-    // const [stockData, setStockData] = useState();
-    const [stockPrice, setStockPrice] = useState();   
-    
     useEffect(() => {
-        console.log("THIS THIS THIS")
         var axios = require("axios").default;
-
         var options = {
             method: 'GET',
             url: URL,
@@ -30,32 +25,36 @@ const StockAPI = (stockTicker) => {
                 'x-api-key': APIKEY,
             }
         };
-
         const delayDebounceFn = setTimeout(() => {
-            // console.log("HERE HERE HERE!!!")
-            // Send Axios request here
             axios.request(options).then(function (response) {
-                // console.log("HERE!")
-                // console.log(response.data);
                 let APIdata = response.data.quoteResponse.result[0].ask;
-                // console.log()
+                let stockName = response.data.quoteResponse.result[0].longName;
+                let stockSym = response.data.quoteResponse.result[0].symbol;
                 setStockPrice(APIdata)
-                // console.log("THIS IS stock PRICE: ", stockPrice);
+                setStockName(stockName)
+                setStockSymbol(stockSym);
             }).catch(function (error) {
                 console.error(error);
             });
-          }, 3000)
-      
-          return () => clearTimeout(delayDebounceFn)
-        
+          }, 1000)
+          return () => clearTimeout(delayDebounceFn)  
     },[URL, stockPrice]);
 
     if (stockPrice === undefined) {
-        // console.log("Stock price is undefined. If error is 429 then too many request have been made.")
-        return "loading..."
+        return (
+            <div style= {{paddingTop: '15px'}}>
+                <p>Loading...</p>
+            </div>
+        );
     }
     else {
-        // console.log("everything is fine!")
-        return stockPrice
+        // 1.125 rem
+        // 2 rem
+        return (
+            <div style= {{paddingTop: '15px'}}>
+                <p>{stockLongName} ({stockSymbol})</p>
+                <p style={{fontSize: '2rem'}}>${stockPrice}</p>
+            </div>
+        );
     }
 }; export default StockAPI;
--- src/components/dashboard/budget.js
+++ src/components/dashboard/budget.js
@@ -1,20 +1,27 @@
 import React, { useEffect,useState } from 'react';
-import { Box, Card, CardContent, Grid, Typography, TextField, InputAdornment, SvgIcon } from '@mui/material';
-// import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-// import MoneyIcon from '@mui/icons-material/Money';
+import { Box, Card, CardContent, Avatar, Grid, Typography, TextField, InputAdornment, SvgIcon } from '@mui/material';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import MoneyIcon from '@mui/icons-material/Money';
 import StockAPI from 'src/API/stocksAPI';
 import { Search as SearchIcon } from '../../icons/search';
+// import StockSearchBar from '../StockSearchBar';
 
 export const Budget = (props) => {
-  const [stock, setStock] = useState(); 
+  const [stock, setStock] = useState();
+
+  console.log("THIS IS STOCK: ", stock);
 
 
   // This will hopefully lag the user input....s
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
       console.log("HERE HERE HERE!!!")
+      if (stock === undefined || stock === '') {
+        setStock('VOO');
+      }
+
       // Send Axios request here
-    }, 3000)
+    }, 1000)
 
     return () => clearTimeout(delayDebounceFn)
   }, [stock])
@@ -25,15 +32,12 @@ export const Budget = (props) => {
     setStock(e.target.value.toUpperCase());
   }
 
-  
-
-  return (
-    <div>
-      <Box sx={{ mt: 0 }}>
-          <Card>
-            <CardContent>
-              
-              <Box sx={{ maxWidth: 500 }}>
+  const searchBarUtil = () => {
+    return (
+      <div>
+        <Box sx={{ mt: 0 }}>
+              {/* Search in the bar to find a stock price */}
+              <Box sx={{ maxWidth: 600 }}>
                 <TextField
                   fullWidth
                   onChange={onInputChange}
@@ -44,7 +48,7 @@ export const Budget = (props) => {
                           color="action"
                           fontSize="small"
                         >
-                          <SearchIcon />
+                        <SearchIcon />
                         </SvgIcon>
                       </InputAdornment>
                     )
@@ -53,76 +57,121 @@ export const Budget = (props) => {
                   variant="outlined"
                 />
               </Box>
-            </CardContent>
-          </Card>
-        </Box>
+            </Box>
+          
+      </div>
+      
+    );
+  }
 
+  
 
+  return (
+    <div>
       <Card
-        sx={{ height: '100%' }}
-        {...props}
+    sx={{ height: '100%' }}
+    {...props}
+  >
+    <CardContent>
+      <Grid
+        container
+        spacing={3}
+        sx={{ justifyContent: 'space-between' }}
       >
-        <CardContent>
-          <Grid
-            container
-            spacing={3}
-            sx={{ justifyContent: 'space-between' }}
+        <Grid item>
+
+          <Typography
+            color="textSecondary"
+            gutterBottom
+            variant="overline"
           >
-            <Grid item>
-              <Typography
-                color="textSecondary"
-                gutterBottom
-                variant="overline"
-              >
-                Stock Data
-              </Typography>
-              <Typography
-                color="textPrimary"
-                variant="h4"
-              >
-                {/* IMPLEMENTING THE STOCK API */}
-                {/* <StockAPI stockTicker={"AAPL"}/>  */}
-                <StockAPI stockTicker = {stock}/>
-              </Typography>
-            </Grid>
-            <Grid item>
-              {/* <Avatar
-                sx={{
-                  backgroundColor: 'error.main',
-                  height: 56,
-                  width: 56
-                }}
-              >
-                <MoneyIcon />
-              </Avatar> */}
-            </Grid>
-          </Grid>
-          {/* <Box
-              sx={{
-                pt: 2,
-                display: 'flex',
-                alignItems: 'center'
-              }}
-            >
-              <ArrowDownwardIcon color="error" />
-              <Typography
-                color="error"
-                sx={{
-                  mr: 1
-                }}
-                variant="body2"
-              >
-                12%
-              </Typography>
-              <Typography
-                color="textSecondary"
-                variant="caption"
-              >
-                Since last month
-              </Typography>
-          </Box> */}
-        </CardContent>
-      </Card>
+            Stock Price
+          </Typography>
+          <Typography
+            color="textPrimary"
+            variant="h6"
+          >
+        <Box
+          sx={{
+           alignItems: 'center',
+            display: 'flex',
+            pt: 2
+          }}
+        >
+          
+          {/* <NationalDebtAPI displayData={"nationalDebt"}/> */}
+          <Box sx={{ mt: 0 }}>
+            {/* Search in the bar to find a stock price */}
+              <Box sx={{ maxWidth: 600 }}>
+                <TextField
+                  fullWidth
+                  onChange={onInputChange}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SvgIcon
+                          color="action"
+                          fontSize="small"
+                        >
+                        <SearchIcon />
+                        </SvgIcon>
+                      </InputAdornment>
+                    )
+                  }}
+                  placeholder="Search stock"
+                  variant="outlined"
+                />
+              </Box>
+            </Box>
+          </Box>
+          <StockAPI stockTicker={stock} />
+          </Typography>
+        </Grid>
+        <Grid item>
+          {/* <Avatar
+            sx={{
+              backgroundColor: 'warning.main',
+              height: 56,
+              width: 56
+            }}
+          >
+            <InsertChartIcon />
+          </Avatar> */}
+        </Grid>
+      </Grid>
+      <Box
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          pt: 3
+        }}
+      >
+        <Typography
+          color="textSecondary"
+          variant="caption"
+        >
+          {/* National Debt Publish Date */}
+        </Typography>
+        </Box>
+        <Box
+           sx={{
+            alignItems: 'center',
+            display: 'flex',
+            pt: 2
+          }}
+        >
+        {/* <DateRangeIcon /> */}
+        <Typography
+          variant="caption"
+          sx={{
+            mr: 1
+          }}
+        >
+          {/* <NationalDebtAPI displayData={"recordDate"}/> */}
+        </Typography>
+     </Box>
+    </CardContent>
+  </Card>
     </div>
   );
 
